721538debed4978f14ac851889566c3c98042687
Maximus <maximus.cruz@penedo.ufal.br>
2026-06-25 11:01:34 -0300

    feat: Data insertion function for the table added

--- pipeline/database.py
+++ pipeline/database.py
@@ -9,3 +9,13 @@ def create_table():
     cursor.execute('CREATE TABLE IF NOT EXISTS rejected_sales (order_id TEXT, customer_id TEXT, product_name TEXT, quantity TEXT, unit_price TEXT, sale_date TEXT, payment_method TEXT, city TEXT)')
     conn.commit()
     conn.close()
+
+def insert_data(approved_sales, rejected_sales):
+    conn = sqlite3.connect(pathdb)
+    cursor = conn.cursor()
+    for sale in approved_sales:
+        cursor.execute('INSERT INTO sales VALUES(?, ?, ?, ?, ?, ?, ?, ?)', (sale['order_id'], sale['customer_id'], sale['product_name'],sale['quantity'], sale['unit_price'], sale['sale_date'], sale['payment_method'], sale['city']))
+    for sale in rejected_sales:
+        cursor.execute('INSERT INTO sales VALUES(?, ?, ?, ?, ?, ?, ?, ?)',(sale['order_id'], sale['customer_id'], sale['product_name'], sale['quantity'],sale['unit_price'], sale['sale_date'], sale['payment_method'], sale['city']))
+    conn.commit()
+    conn.close()
