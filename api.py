7f280130c6d5d093a37138e79ff71fb5f0a0195e
Maximus <maximus.cruz@penedo.ufal.br>
2026-06-28 14:53:15 -0300

    feat: add endpoint /sales/city/{city}

--- api.py
+++ api.py
@@ -17,12 +17,19 @@ def get_sales():
     conn.close()
     return data
 
-def get_sale_by_id(id):
+def get_sale_by_id(id: int):
     conn = sqlite3.connect(path_db)
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM sales WHERE order_id = ?', (id,))
     data = row_to_dict(cursor)
     conn.close()
+    return data
+
+def get_sale_by_city(city: str):
+    conn = sqlite3.connect(path_db)
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM sales WHERE city = ?', (city,))
+    data = row_to_dict(cursor)
     return data
 
 @app.get('/sales')
@@ -31,7 +38,11 @@ def list_sales():
     return sales
 
 @app.get('/sales/{order_id}')
-def list_get_sales(order_id: int):
+def list_get_sales_id(order_id: int):
     data = get_sale_by_id(order_id)
     return data
 
+@app.get('/sales/city/{city}')
+def list_get_sales_city(city: str):
+    data = get_sale_by_city(city)
+    return data
